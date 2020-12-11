4a206a7426e10b5b7ac45e41cc252998607a2a75
CCooper92 <am10ac17@gmail.com>
2020-12-11 10:31:26 -0800

    finished mainly just need to rezise images

--- README.md
+++ README.md
@@ -23,7 +23,15 @@ Commit your code regularly and meaningfully. This helps both you (in case you ev
 Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics. You might prepare by writing down your own answers before hand.
 
 1. How would you describe acessibility on the web to someone new to programming?
+
+lets screen readers no exactly what the webpage holds whether its a header footer body paragraph or image and what the image is of.
+
 2. Talk about 3 different things you can do to ensure your website is accessible. 
+
+semantic html
+alt tag on images
+compatible with different screen sizes
+
 3. How would you explain the concept of a variable to someone new to programming?
 4. What is the purpose of using functions in code?
 
--- index.js
+++ index.js
@@ -228,8 +228,10 @@ There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is current
  
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array, index) {
+  if (array [index]){
+    return `the artist at index ${[index]} is ${array[index].name}`;
+  }
 }  
 
 
@@ -242,9 +244,17 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(array){
+  const newArray=[];
+  for (let i = 0; i < array.length; i++){
+    if(array[i].years >= '1900' && array[i].years < '2000'){
+      newArray.push(array[i].name);
+    }
+  }
+  return newArray;
 }
+
+
 
 
 
@@ -257,8 +267,9 @@ function get20s(/*Your Code Here*/){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(array, index){
+   array.splice(array.indexOf(index),1);
+   return array.length;
 }
    
 
@@ -278,9 +289,11 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+function addArtist(array, id, name, years, genre, nationality, bio){
+  array.push({id, name, years, genre, nationality, bio});
+  return array;
+}
+    
 
   
 
@@ -291,8 +304,15 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+function lotsOfArt(array){
+  const updatedArray=[];
+  for (let i = 0; i < array.length; i++){
+    if(array[i].paintings > 100){
+      updatedArray.push(array[i].name);
+    
+    }
+  }
+  return updatedArray;
 }
 
 
